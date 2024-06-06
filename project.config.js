d8b02f459976a7c9ae3b93d766bbf2fc9a39a518
不傻的别来 <51734480+wcwtfhvk@users.noreply.github.com>
2024-06-06 09:00:47 +0800

    Update project.config.js
    阿里云的报错试试删了会不会好

--- project.config.js
+++ project.config.js
@@ -2,10 +2,10 @@ let config = {
   title: "网页标题", // 网页标题
   useCdnUrl: true, //是否使用svn绝对路径
   baiduUrl: "", // 百度统计链接
-  cdnUrl: "//cdn.jzzz66.cn/weipyq",
-  grayCdnUrl: "//cdn.jzzz66.cn/weipyq",
-  testCdnUrl: "//cdn.jzzz66.cn/weipyq",
-  innerCdnUrl: "//cdn.jzzz66.cn/weipyq",
+  cdnUrl: "",
+  grayCdnUrl: "",
+  testCdnUrl: "",
+  innerCdnUrl: "",
   forceHttps: false,
 };
 
